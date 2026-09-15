4d7946578f9292477b5421610177a959b309b7ed
abrarr21 <abrarrr2110@gmail.com>
2026-09-15 14:03:17 +0530

    feat: add proxy and tailwind

--- web/dashboard/vite.config.ts
+++ web/dashboard/vite.config.ts
@@ -1,7 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:80",
+      "/ws": { target: "ws://localhost:80", ws: true },
+    },
+  },
+});
